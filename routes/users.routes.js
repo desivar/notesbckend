41a64bb1de8aa58a4e9871ad3d98772209d6743d
desivar <jillian_desire@yahoo.es>
2025-04-09 09:12:52 -0600

    simplify

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,22 +1,9 @@
 import express from "express";
 const router = express.Router();
 
-import {
-  handleUserLogout as usersHandleUserLogout, // Alias the logout from users controller
-  handleUserSignup,
-} from "../controllers/users.controllers.js";
-
-import {
-  signin as handleUserSignin // Alias the signin from auth controller
-} from "../controllers/authController.js";
-
-//  SIGN-UP USER ROUTE      /users/sign-up
-router.post("/sign-up", handleUserSignup);
+import { signin as handleUserSignin } from "../controllers/authController.js";
 
 // SIGN-IN USER ROUTE      /users/sign-in
 router.post("/sign-in", handleUserSignin);
-
-//  LOGOUT USER ROUTE      /users/logout
-router.post("/logout", usersHandleUserLogout); // Use the aliased logout
 
 export default router;
