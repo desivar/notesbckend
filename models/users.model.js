a78fb6af2f3dbcfb56e41804274198cbe1e52045
desivar <jillian_desire@yahoo.es>
2025-04-09 03:22:43 -0600

    fix error hanling singing up

--- models/users.model.js
+++ models/users.model.js
@@ -2,37 +2,36 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
-  {
-    fullName: {
-      type: String,
-      required: true,
+    {
+        fullName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 // PASSWORD HASHING
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-
-  this.password = await bcrypt.hash(this.password, 10);
-  return next();
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    return next();
 });
 
-// COMPARE PASSWORD
-userSchema.methods.comparePassword = function (userPassword) {
-  return bcrypt.compare(userPassword, this.password);
+// METHOD TO COMPARE PASSWORD DURING SIGNIN
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
-
+// Export the User model (assuming you haven't already)
+const User = mongoose.model('User', userSchema);
 export default User;
