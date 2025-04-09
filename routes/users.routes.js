88c356dc67de8850867f6e8f8c4b14e6f32839fe
desivar <jillian_desire@yahoo.es>
2025-04-09 09:11:23 -0600

    different sign in

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
+const router = express.Router();
+
 import {
-  handleUserLogout,
+  handleUserLogout as usersHandleUserLogout, // Alias the logout from users controller
   handleUserSignup,
 } from "../controllers/users.controllers.js";
-import {
-  signin as handleUserSignin // Import the signin function from authController and alias it
-} from "../controllers/authController.js";
 
-const router = express.Router();
+import {
+  signin as handleUserSignin // Alias the signin from auth controller
+} from "../controllers/authController.js";
 
 //  SIGN-UP USER ROUTE      /users/sign-up
 router.post("/sign-up", handleUserSignup);
@@ -16,6 +17,6 @@ router.post("/sign-up", handleUserSignup);
 router.post("/sign-in", handleUserSignin);
 
 //  LOGOUT USER ROUTE      /users/logout
-router.post("/logout", handleUserLogout);
+router.post("/logout", usersHandleUserLogout); // Use the aliased logout
 
 export default router;
