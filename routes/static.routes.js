a689ade6870e735be2de832adadae2c70a08d143
desivar <jillian_desire@yahoo.es>
2025-04-09 12:47:30 -0600

    testing

--- routes/static.routes.js
+++ routes/static.routes.js
@@ -1,14 +1,10 @@
 // THIS STATIC.ROUTES.JS FILE CONTAINS THE CODE THAT RENDER VIEWS TO THE BROWSER
 import express from "express";
-import { setAuthStatus } from "../middlewares/setAuthStatus.js";
 import { authenticated } from "../middlewares/authenticated.js";
 
 const router = express.Router();
 
-// RENDER HOME PAGE TO "/" ROOT PATH
-router.get("/", setAuthStatus, async (req, res) => {
-  res.render("home", { authenticated: req.isAuthenticated });
-});
+
 
 // RENDER SIGN-IN PAGE TO "/sign-in" PATH
 router.get("/sign-in", (req, res) => {
