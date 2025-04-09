b61a1a96873e261f0d593a66a34add91e0f6e3d7
desivar <jillian_desire@yahoo.es>
2025-04-09 07:44:43 -0600

    get rid of duplicated code

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ import { app } from "./app.js"; // Assuming your Express app configuration is in
 dotenv.config({
   path: "./.env",
 });
-import dotenv from 'dotenv';
-dotenv.config({ path: './.env' });
-console.log("JWT_SECRET from env:", process.env.JWT_SECRET); 
+console.log("JWT_SECRET from env:", process.env.JWT_SECRET);
 
 const PORT = process.env.PORT || 5500;
 
