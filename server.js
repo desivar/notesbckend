c3b47649ce1b87355bf6d6429372a1a57ce1c1fe
desivar <jillian_desire@yahoo.es>
2025-04-09 12:54:22 -0600

    testing

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ import mongoose from "mongoose";
 import { app } from "./app.js"; // Import the app
 
 const PORT = process.env.PORT || 5500; // Use your desired port
+const mongoUri = process.env.MONGODB_URI; // Access the variable directly
 
 // CONNECTION WITH DATABASE & RUN SERVER
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(mongoUri) // Use the explicitly accessed variable
   .then(() => {
     console.log(" MongoDB connected !! DB HOST:", mongoose.connection.host);
     app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
