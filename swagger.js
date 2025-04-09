fb19582e16ec6271ba705420ddb7ae582b176c17
desivar <jillian_desire@yahoo.es>
2025-04-09 05:03:51 -0600

    swagger.js

---
+++ swagger.js
@@ -0,0 +1,24 @@
+// swagger.js
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+    info: {
+        title: 'Your API Title',
+        description: 'Description of your API',
+    },
+    host: `localhost:${process.env.PORT || 5500}`, // Use the same port as your server
+    schemes: ['http'], // Or ['http', 'https'] if you use HTTPS
+    securityDefinitions: {
+        bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+        }
+    },
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./routes/auth.js', './routes/users.routes.js', './routes/notes.routes.js', './routes/static.routes.js'];
+// Add paths to ALL your route files here
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
